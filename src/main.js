d753a84658c366e3fd3c685e2a6179833a2dfb04
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-04-26 21:07:20 +0200

    Add dependencies for handling axios.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from './App';
 import router from './router';
@@ -9,6 +11,7 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
