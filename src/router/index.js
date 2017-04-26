90b706e2bfbe0e947d6087c0dc95adc6a13ef33f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-04-26 22:20:41 +0200

    Add routes and component files.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import Login from '@/components/Login';
+import Settings from '@/components/Settings';
+import Editor from '@/components/Editor';
+import Article from '@/components/Article';
+import Profile from '@/components/Profile';
+import Favorites from '@/components/Favorites';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
+    { path: '/', name: 'home', component: Home },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/signup', name: 'register', component: Login },
+    { path: '/settings', name: 'settings', component: Settings },
+    { path: '/editor', name: 'create-article', component: Editor },
+    { path: '/editor/:id', name: 'edit-article', component: Editor },
+    { path: '/article/:id', name: 'article', component: Article },
+    { path: '/profile/:username', name: 'user', component: Profile },
+    { path: '/article/:username/favorites', name: 'favorites', component: Favorites },
   ],
 });
