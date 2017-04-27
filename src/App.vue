9231f20e57eaf8499c7ff461e82c1c7d455165c4
Matt Chandler <mcaz689@gmail.com>
2017-04-27 13:04:06 -0700

    add RealWorldHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <real-world-header />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import RealWorldHeader from '@/components/Header';
+
 export default {
   name: 'app',
+  components: {
+    RealWorldHeader,
+  },
 };
 </script>
 
