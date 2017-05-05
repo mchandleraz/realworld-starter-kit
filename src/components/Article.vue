800a429f9303796f95561d4f07e6df6fb35ce8a4
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-05-05 21:00:03 +0200

    Remove some code. Show tags on Article page.

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -35,8 +35,11 @@
           <p>
           {{ article.body }}
           </p>
-          <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-          <p>It's a great solution for learning how other frameworks work.</p>
+          <ul class="tag-list">
+            <li class="tag-default tag-pill tag-outline" v-for="tag in article.tagList">
+              {{ tag }}
+            </li>
+        </ul>
         </div>
       </div>
 
@@ -69,24 +72,6 @@
               &nbsp;
               <a href="" class="comment-author">Jacob Schmidt</a>
               <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-              <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-              </span>
             </div>
           </div>
           
