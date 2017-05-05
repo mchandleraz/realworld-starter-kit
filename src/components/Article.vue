9d4d60b453a5df3939cc82927b5757826ce6c6fb
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-05-05 20:52:15 +0200

    Removing unused code

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -42,28 +42,6 @@
 
       <hr />
 
-      <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
-      </div>
-
       <div class="row">
 
         <div class="col-xs-12 col-md-8 offset-md-2">
