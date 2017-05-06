283b97285fe4a478a643a5e4aa3f1bf691101cb1
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-05-06 18:35:33 +0200

    Add functions for fetching articles by user and favorite articles.

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -7,4 +7,16 @@ export default {
       throw new Error(error);
     });
   },
+  getUserArticles(username) {
+    return Vue.axios.get(`https://conduit.productionready.io/api/profiles/${username}?author=${username}`)
+    .catch((error) => {
+      throw new Error(error);
+    });
+  },
+  getUserFavorites(username) {
+    return Vue.axios.get(`https://conduit.productionready.io/api/profiles/${username}?favorited=${username}`)
+    .catch((error) => {
+      throw new Error(error);
+    });
+  },
 };
