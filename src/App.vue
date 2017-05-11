0f1f4cf84842a7ef2d0bce5feda4161ebdef5fae
Michal <michalzagrodzki@gmail.com>
2017-05-11 06:06:04 +0200

    Add footer (#4)
    * Create structure for footer.
    
    * Add template from realworld-starter-kit
    
    * Replace href link with router-link

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <real-world-header />
     <router-view></router-view>
+    <real-world-footer />
   </div>
 </template>
 
 <script>
 import RealWorldHeader from '@/components/Header';
+import RealWorldFooter from '@/components/Footer';
+
 
 export default {
   name: 'app',
   components: {
-    RealWorldHeader,
+    RealWorldHeader, RealWorldFooter,
   },
 };
 </script>
