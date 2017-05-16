52b624b4e7e621e6a6132dd18f0327cebba77185
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2017-05-17 02:40:22 +0800

    create api service

--- src/constants/Home.constants.js
+++ src/constants/Home.constants.js
@@ -2,3 +2,5 @@ export default {
   BANNER_MESSAGE: 'A place to share your knowledge.',
   HTTP_OK: 200,
 };
+
+export const API_URL = 'https://conduit.productionready.io/api';
---
+++ src/services/apiService.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import { API_URL } from '@/constants/Home.constants';
+
+export default class ApiService {
+
+  static get(resource, params = '') {
+    return Vue.axios
+      .get(`${API_URL}/${resource}/${params}`)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+}
