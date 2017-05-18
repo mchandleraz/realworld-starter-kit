5b84beda111fcfedd1743b17ea94d5f788b2968f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-05-18 22:17:47 +0200

    Set actions and mutations for fetching and setting data of User.

--- src/store/mutationTypes.js
+++ src/store/mutationTypes.js
@@ -1,2 +1,5 @@
 export const SET_ARTICLES = 'setArticles';
 export const SET_TAGS = 'setTags';
+export const SET_USER = 'setUsers';
+export const SET_USER_ARTICLES = 'setUserArticles';
+export const SET_USER_FAVORITES = 'setUserFavorites';
