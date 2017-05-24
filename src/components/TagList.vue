9ed0542d661c58bdfc1596cff156dbfd10859cf6
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2017-05-25 00:45:49 +0800

    tag-default instead of label-default (#16)

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tag-list">
-    <a v-for="(tag, index) in tags" href="" class="label label-pill label-default">{{tag}}</a>
+    <a v-for="(tag, index) in tags" href="" class="tag-pill tag-default">{{tag}}</a>
   </div>
 </template>
 
