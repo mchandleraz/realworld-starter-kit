0c2c7e5532a97548036d9237d485cd6e507a2619
Matt Chandler <mcaz689@gmail.com>
2017-05-23 23:33:46 -0700

    add json coverage reporter

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' },
+        {type:'json', subdir: '.'},
       ]
     },
   });
